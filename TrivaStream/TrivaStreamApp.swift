b99cd40ebbd495ce87e76e43f3ac20e20e7e6593
Meet Brahmbhatt <meetbrahmbhatt91@gmail.com>
2026-09-17 15:06:40 +0530

    Initial Commit

---
+++ TrivaStream/TrivaStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivaStreamApp.swift
+//  TrivaStream
+//
+//  Created by Meet Brahmbhatt on 17/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TrivaStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
